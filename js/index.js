fcb04b49e709bb5e571243f07b91ac92a2653396
MrMernDeveloper <rakibkst16@gmail.com>
2022-09-02 23:49:59 +0600

    complete li section

--- js/index.js
+++ js/index.js
@@ -1,1 +1,38 @@
-console.log('connected')
+
+const loadData = async () => {
+    const url = "https://openapi.programming-hero.com/api/news/categories"
+    const res = await fetch(url);
+    const data = await res.json();
+    displayCategory(data.data.news_category)
+
+}
+
+const displayCategory = (categories) => {
+    const ul = document.getElementById("ul-container");
+    
+    categories.forEach(category => {
+        const { category_name, category_id } = category;
+        console.log(category)
+        const li = document.createElement('li');
+        li.classList.add('d-inline-block')
+        li.innerHTML = ` <li onclick="loadSingleCategory(${category_id})"class="d-inline-block">${category_name}</li>`
+        ul.appendChild(li);
+    })
+    
+    
+}
+
+const loadSingleCategory = async (id) => {
+    const url = `https://openapi.programming-hero.com/api/news/category/0${id}`
+    const res = await fetch(url);
+    const data = await res.json();
+    console.log(data.data);
+
+}
+
+// const displayAllCategory = (dataBYId) => {
+//     console.log(dataBYId);
+    
+// }
+
+loadData();
