0b94db7c39ee22c51a1f9a81b26f2531f55f2796
MrMernDeveloper <rakibkst16@gmail.com>
2022-09-02 23:57:09 +0600

    catch single news

--- js/index.js
+++ js/index.js
@@ -26,13 +26,14 @@ const loadSingleCategory = async (id) => {
     const url = `https://openapi.programming-hero.com/api/news/category/0${id}`
     const res = await fetch(url);
     const data = await res.json();
-    console.log(data.data);
+    displayAllCategory(data.data);
 
 }
 
-// const displayAllCategory = (dataBYId) => {
-//     console.log(dataBYId);
+const displayAllCategory = async (newses) => {
+    console.log(newses);
+
     
-// }
+}
 
 loadData();
